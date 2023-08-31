ac64ff4c5dc864d9dd708d5cebfaac155896ce14
Jesus Aldrete <jesus@aldrete.pro>
2023-08-31 14:07:24 -0600

    🛑 : corrección del botón del menu

--- components/Menu/Menu_button.tsx
+++ components/Menu/Menu_button.tsx
@@ -7,7 +7,13 @@ interface Props {
 
 export default function Menu_button({ children, active }:Props) {
 	return (
-		<button className={`text-sm text-[#A3A3A3] py-[10px] ${ active ? 'text-[#1B1B18] border-b-[2px] border-[#1b1b18]' : '' }`}>
+		<button
+			className='text-sm text-[#A3A3A3] py-[10px]'
+			style={{
+				borderBottom: active ? 'solid 2px #1b1b18' : '',
+				color       : active ? '#1B1B18'           : '#A3A3A3',
+			}}
+		>
 			{children}
 		</button>
 	);
