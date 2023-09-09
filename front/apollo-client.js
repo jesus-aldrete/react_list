f683178671b128220508d248ce057b78f8ec0e93
Jesus Aldrete <jesus@aldrete.pro>
2023-09-08 20:51:14 -0600

    ☢️ front: se elmina el consumo de strapi por costos

--- front/apollo-client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-	uri: "https://best-birds-5c1a4927fc.strapiapp.com/graphql",
-	cache: new InMemoryCache(),
-});
-
-export default client;
--- front/components/Data.tsx
+++ front/components/Data.tsx
@@ -1,26 +1,40 @@
-import { gql } from "@apollo/client";
-import client from "../apollo-client";
-import { IDataRow, IQueryResultContractors } from './Types';
-
 export async function GetContractors() {
-	const { data:{ contractors } }:IQueryResultContractors = await client.query({
-		query: gql`{
-			contractors {
-				data {
-					attributes {
-						name
-						specialities
-						day_rate
-						availability
-					}
-				}
-			}
-		}`,
-	});
-
-	return contractors.data.map( ( v ) => {
-		const res = { ...v.attributes };
-
-		return res;
-	});
+	return [
+		{
+			availability: true,
+			day_rate    : 300,
+			name        : 'Strykes',
+			specialities: [ 'Surgery', 'Medical' ],
+		},
+		{
+			availability: false,
+			day_rate    : 250,
+			name        : 'Rossonerri',
+			specialities: [ 'Healthcare', 'Fitness' ],
+		},
+		{
+			availability: false,
+			day_rate    : 199,
+			name        : 'Tyga',
+			specialities: [ 'Surgery', 'Medical' ],
+		},
+		{
+			availability: true,
+			day_rate    : 200,
+			name        : 'Yolo.corp',
+			specialities: [ 'Automotive', 'Modification' ],
+		},
+		{
+			availability: true,
+			day_rate    : 100,
+			name        : 'Bardi',
+			specialities: [ 'Technology', 'Home Living', 'Automotive', 'Modification', 'Surgery', 'Medical', 'Healthcare' ],
+		},
+		{
+			availability: false,
+			day_rate    : 2003,
+			name        : 'prueba',
+			specialities: [ 'tag 1', 'tag 2' ],
+		}
+	];
 }
